1e41b08e94fa25ad09b567966cbd9fdad80ead0f
Krish Makwana <krishmakwanaapop0801@gmail.com>
2026-08-18 18:08:31 -0700

    Add FastAPI endpoint: SUCESSFUL

---
+++ api.py
@@ -0,0 +1,17 @@
+# Imports
+from fastapi import FastAPI
+from models import PatientRequest, RankedOutput
+from pipeline import run_pipeline
+from fastapi.responses import RedirectResponse
+
+app = FastAPI(title="AGENTIS")
+
+@app.get("/", include_in_schema=False)
+async def root():
+    return RedirectResponse("/docs")
+
+
+# endpoint for using agentis
+@app.post("/match-trials", response_model=RankedOutput)
+async def match_trials(patient_info: PatientRequest):
+    return await run_pipeline(patient_info)
